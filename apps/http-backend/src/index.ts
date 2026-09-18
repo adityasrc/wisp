873e0751465557805c44ed2d5939c89c9f59473a
Aditya Prakash <adityaprakash.bih@gmail.com>
2026-09-18 16:37:08 +0530

    feat(backend): add signup & login endpoints

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,17 @@
 import express from "express";
+import cors from "cors";
 const app = express();
-import authRoutes from "./routes/auth.js"
-import roomRoutes from "./routes/room.js"
+import authRoutes from "./routes/auth.js";
+import chatRoutes from "./routes/chats.js";
 
-app.use("/v1/api/auth", authRoutes);
-app.use("v1/api/rooms", roomRoutes)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.listen(3001, ()=> {
+
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/chats", chatRoutes);
+
+app.listen(3001, () => {
     console.log(`Http-backend running at ${3001}`);
 });
--- apps/http-backend/src/routes/chats.ts
+++ apps/http-backend/src/routes/chats.ts
@@ -1,7 +1,7 @@
 import express, { Router } from "express";
 const router: Router = Router();
 
-router.post("/rooms", (req, res) => {
+router.post("/", (req, res) => {
 
 })
 
